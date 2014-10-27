024deb94de634bcf37efa161ad03d4520e63161d
leopoldwe <leopoldwe@gmail.com>
2014-10-27 11:13:26 -0400

    Added documentation
    Added comments to show what the methods do and to make the code more readable.

--- eMail.cpp
+++ eMail.cpp
@@ -5,7 +5,23 @@
 #include <Poco/Net/MailRecipient.h>
 #include <QTCore/QString.h>
 
+/**
+ * Author: Leopold Eckert
+ * Filename: Email.cpp
+ * Contains methods for connecting to SMTP servers and sending email.
+ */
+
 using namespace Poco::Net;
+
+/**
+ * Name: openConnection
+ * Connects to and logins to an SMTP host
+ * Params:
+ *   hostname: the name of the SMTP host to connect to
+ *   user: the username used in login
+ *   pass: password for login
+ * Returns: A Client session that can be used to send emails.
+ */
 SMTPClientSession openConnection(QString hostname, QString user, QString pass) {
 	//Convert QStrings to std strings
 	std::string hostnameString = hostname.toStdString();
@@ -17,14 +33,26 @@ SMTPClientSession openConnection(QString hostname, QString user, QString pass) {
 	result.login(SMTPClientSession::AUTH_LOGIN, userString, passwordString);
 	
 	return result;
-}
+}// End of method: openConnection
 
+/**
+ * Name: sendFile
+ * Sends a file over email as an attachment
+ * Params:
+ *    path: the path to the file to attach
+ *    qRecipient: the email address of the recipient of the file
+ *    client: an client session used to send the file.
+ * 
+ */
 void sendFile(std::string path, QString qRecipient, SMTPClientSession client) {
+	//Get the attachment
 	FilePartSource attachment(path);
 	std::string recipientString = qRecipient.toStdString();
+	//Prepare and send the message
 	MailMessage mail;
 	mail.addAttachment("Encrypted File", &attachment);
 	MailRecipient recipient(MailRecipient::PRIMARY_RECIPIENT, recipientString);
 	mail.addRecipient(recipient);
 	client.sendMessage(mail);
-}
+}// End of method: sendFile
+// End of file: Email.cpp
